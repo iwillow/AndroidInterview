b2859cbc46dcfee4c5013ae44b361a95cfb32336
iwillow <iwillow@163.com>
2017-10-16 12:05:38 +0800

    git add activity notes

---
+++ app/src/main/java/com/iwillow/app/android/interview/SecondActivity.java
@@ -0,0 +1,98 @@
+package com.iwillow.app.android.interview;
+
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = SecondActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        showSize("onStart");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "onConfigurationChanged");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        showSize("onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        showSize("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        showSize("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+        showSize("onRestart");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        showSize("onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState(Bundle outState)");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.d(TAG, "onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState)");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState(Bundle savedInstanceState)");
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        Log.d(TAG, "onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState)");
+    }
+
+    private void showSize(String tag) {
+        // int w = findViewById(R.id.tv_hello).getWidth();
+        //  int h = findViewById(R.id.tv_hello).getHeight();
+        // Log.d(TAG, tag + " Width:" + w + ";h:" + h);
+    }
+
+}
--- notes/activity.md
+++ notes/activity.md
@@ -4,7 +4,7 @@
 
 初次：oncreate->onStart->onResume(可见)。
 
-按home键或锁屏：onPause->onSaveInstanceState(Bundle outState)->onStop(不可见)，（有些手机可能会调用onDestroy）。
+按home键或锁屏：onPause->onSaveInstanceState(Bundle outState)->onStop(不可见)，（有些手机按锁屏键可能会调用onDestroy）。
 
 再回到应用：onRestart->onStart->onResume
 
@@ -12,6 +12,10 @@
 
 如果配置了 android:configChanges="screenSize|orientation"，自从Android 3.2（API 13），screen size也开始跟着设备的横竖切换而改变。所以，在AndroidManifest.xml里设置的MiniSdkVersion和TargetSdkVersion属性大于等于13的情况下，如果你想阻止程序在运行时重新加载Activity，除了设置”orientation“，你还必须设置”ScreenSize”，就像这样子，android:configChanges=”orientation|screenSize”。但是呢，如果你的Target API 级别小于13，你的Activity自己会自动处理这种ScreenSize的变化。如果你的TargetSdkVersion小于13，即使你在Android 3.2或者更高级别的机器上运行程序，它还是会自己去处理ScreenSize的。
 
+如果ActivityA启动ActivityB，两者个周期依次是
+MainActivityA#onPause->MainActivityB#onCreate->MainActivityB#onStart->MainActivityB#onResume->MainActivityA#onSaveInstanceState(Bundle outState)->MainActivityA#onStop
 
+
+## 启动模式
 
 
